e5663ed0fcd1d1f3f43e61cb8aab733e6d8c97c1
Jonathan Howell <jon.howell314@gmail.com>
2022-06-20 08:42:43 +0800

    🌱 created project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ErdViewer />
+</template>
+
+<script setup>
+import ErdViewer from "./components/ErdViewer.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ErdViewer.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="erd-viewer">
+    <div id="diagram"></div>
+  </div>
+</template>
+
+<script>
+import go from 'gojs';
+
+const DIV_NAME = 'diagram';
+
+export default {
+  mounted() {
+    const $ = go.GraphObject.make; // for conciseness in defining templates
+    const myDiagram = $(
+      go.Diagram,
+      DIV_NAME, // create a Diagram for the DIV HTML element
+      {
+        // enable undo & redo
+        'undoManager.isEnabled': true,
+      },
+    );
+
+    // define a simple Node template
+    myDiagram.nodeTemplate = $(
+      go.Node,
+      'Auto', // the Shape will go around the TextBlock
+      $(
+        go.Shape,
+        'RoundedRectangle',
+        { strokeWidth: 0, fill: 'white' }, // default fill is white
+        // Shape.fill is bound to Node.data.color
+        new go.Binding('fill', 'color'),
+      ),
+      $(
+        go.TextBlock,
+        { margin: 8 }, // some room around the text
+        // TextBlock.text is bound to Node.data.key
+        new go.Binding('text', 'key'),
+      ),
+    );
+
+    // but use the default Link template, by not setting Diagram.linkTemplate
+
+    // create the model data that will be represented by Nodes and Links
+    myDiagram.model = new go.GraphLinksModel(
+      [
+        { key: 'Alpha', color: 'lightblue' },
+        { key: 'Beta', color: 'orange' },
+        { key: 'Gamma', color: 'lightgreen' },
+        { key: 'Delta', color: 'pink' },
+      ],
+      [
+        { from: 'Alpha', to: 'Beta' },
+        { from: 'Alpha', to: 'Gamma' },
+        { from: 'Beta', to: 'Beta' },
+        { from: 'Gamma', to: 'Delta' },
+        { from: 'Delta', to: 'Alpha' },
+      ],
+    );
+  },
+};
+</script>
+
+<style scoped>
+#diagram {
+  width: 100%;
+  height: 500px;
+}
+</style>
